6bc4cc44fe3219cb9ceceed7f5e48b288de82a32
Owen McGrath <owen.c.mcgrath@icloud.com>
2024-08-21 09:19:17 -0500

    Delete Library/Mobile Documents/com~apple~CloudDocs/Desktop/Home/Personal directory

--- Library/Mobile Documents/com~apple~CloudDocs/Desktop/Home/Personal/Computer Programming/Swiftlet/Sources/main.swift
+++
@@ -1,9 +0,0 @@
-func main()
-{
-    let userMenuInstance = UserMenu()
-    userMenuInstance.checkForDirectory()
-    userMenuInstance.getUserActionChoice()
-    userMenuInstance.sendUserBasedOnAction(choice: choice, setName: String, setQuestionsDictionary: [Int: CreateSet.SetQuestions])
-}
-
-main()
